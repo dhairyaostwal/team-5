e10ed96af4bd6628b834dc27f5b7511e2d4dba69
Dhairya <dhairya.ostwal@gmail.com>
2022-06-04 17:29:16 +0530

    Frontend:NeedsHelp & LandingContainer1 Completed

--- front-end/susmafia/src/Landing.js
+++ front-end/susmafia/src/Landing.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import FinancialNews from './components/FinancialNews';
+import NeedHelp from './components/NeedHelp';
 import WebsiteStatistics from './components/WebsiteStatistics';
+import './Landing.css';
 
 const Landing = () => {
   return (
     <div>
-      <FinancialNews />
+      <div className="Landing--container1">
+        <FinancialNews />
+        <NeedHelp />
+      </div>
       <WebsiteStatistics />
     </div>
   );
---
+++ front-end/susmafia/src/components/NeedHelp.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './NeedsHelp.css';
+const NeedHelp = () => {
+  return (
+    <div className="NeedsHelp--Container">
+      <h1>Needs Help</h1>
+
+      <p>
+        <h3>
+          <a href="">Rahul</a> needs help with an Energy Investor
+        </h3>
+        <br />
+        <h3>
+          <a href="">Rahul</a> needs help with an Energy Investor
+        </h3>
+        <br />
+        <h3>
+          <a href="">Rahul</a> needs help with an Energy Investor
+        </h3>
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default NeedHelp;
