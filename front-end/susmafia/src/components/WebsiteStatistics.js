fb4c1a1571d086dec45f3f17512f7c08c0b4f54f
Dhairya <dhairya.ostwal@gmail.com>
2022-06-05 01:43:48 +0530

    CSS Fixes Landing Page

--- front-end/susmafia/src/components/WebsiteStatistics.js
+++ front-end/susmafia/src/components/WebsiteStatistics.js
@@ -5,11 +5,11 @@ import './WebsiteStatistics.css';
 const WebsiteStatistics = () => {
   return (
     <div className="WebsiteStatistics--container">
-      <div style={{ backgroundColor: 'grey', padding: '1% 3%' }}>
-        <h2>Total Users: 576 </h2>
+      <div style={{ backgroundColor: 'black', padding: '1% 3%' }}>
+        <h2 style={{ fontWeight: 600 }}>Total Users: 576 </h2>
         <h2>
-          Change in User Growth: <span style={{ color: '#00ff00' }}>50% </span>
-          <i class="fa-solid fa-triangle" />
+          Change in User Growth:{' '}
+          <span style={{ fontWeight: 600, color: '#00ff00' }}>50% +</span>
         </h2>
       </div>
 
