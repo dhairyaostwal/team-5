83bc8361dbb6409b4bd31349b8b889c5601a41ea
meetagrawal09 <agrawalmeet91@gmail.com>
2022-06-05 00:11:22 +0530

    admin integrated to backend

--- front-end/susmafia/src/AdminPage.js
+++ front-end/susmafia/src/AdminPage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function AdminPage() {
+
+
+    const [users,setUsers] = useState({})
+
+
+    useEffect(()=>{
+        const url = 'http://localhost:8000/api/admin/'
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setUsers(data))  
+        .catch(err=>console.log(err))
+    },[users])
+
+
+    const handleClick = (id) => {
+        const url = `http://localhost:8000/api/admin/admit-user/${id}`
+        fetch(url,{
+            method:'POST'
+        })
+        .then(res=>res.json())
+        .then(data=>setUsers(data))  
+        .catch(err=>console.log(err))
+    }
+
+
   return (
     <div>
         <h1>Admin Page</h1>
+        {users.map((user)=>{
+            <div>
+                <p>{user.name}</p>
+                <p>{user.email}</p>
+                <button onClick={()=>handleClick(user._id)}>Admit</button>
+            </div>
+
+        })}
+
     </div>
   )
 }
