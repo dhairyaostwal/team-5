e24a02739271fcfa689a21d2445ad75d704fbc44
HASAN-SHAIK <skhasanj300@gmail.com>
2022-06-04 23:30:56 +0530

    Modified routes

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Landing from './Landing';
 import NavbarComponent from './components/Navbar';
-
+import ViewProfile from './components/ViewProfile';
 import Login from './Login';
 import Signup from './Signup';
-
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
@@ -14,6 +14,13 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/landing" element={<Landing/> }/>
+               
+        
+        
+
+
+
         <Route path="/" element={<Signup/>}/>
       </Routes>
     </div>
