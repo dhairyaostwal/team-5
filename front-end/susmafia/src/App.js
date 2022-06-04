12e630ad8e53384f790de3d67fe63bddcb63f2d2
Dhairya <dhairya.ostwal@gmail.com>
2022-06-04 21:36:54 +0530

    Frontend: Bootstrap Fix

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
 import Landing from './Landing';
-import { Navbar,Container,Nav, NavLink } from 'react-bootstrap';
-import {Routes,Route} from 'react-router-dom';
+import NavbarComponent from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="dark" variant="dark">
-      <Container>
-      <Navbar.Brand to="">Team -5</Navbar.Brand>
-      <Nav className="mx -auto">
-        <NavLink className='nav-link' to='redeempoints'>Redeeem points</NavLink>
-        <NavLink className='nav-link' to='viewprofile'>View Profile</NavLink>
-      </Nav>
-      </Container>
-    </Navbar>
+      <NavbarComponent />
       <Landing />
 
-
-    {/* <Routes>
+      {/* <Routes>
       <Route path="viewprofile" element={<Viewprofile/>}/>
     </Routes> */}
-
     </div>
   );
 }
