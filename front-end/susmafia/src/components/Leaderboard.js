2da1dc7e0b01f4c50bced28c6f284b2c1dab4e31
Dhairya <dhairya.ostwal@gmail.com>
2022-06-04 19:16:19 +0530

    Frontend: Leaderboard Setup

---
+++ front-end/susmafia/src/components/Leaderboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Leaderboard = () => {
+  return (
+    <div>
+      <h1>Top 10 Entrepreneurs</h1>
+
+      <div>
+        <table>
+          <tr>
+            <td>Rank</td>
+            <td>Name</td>
+            <td>Sector</td>
+            <td>SusDAO</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Dhairya</td>
+            <td>Software</td>
+            <td>10000</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Leaderboard;
