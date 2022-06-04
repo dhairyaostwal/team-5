e92c5d6e639488d3f3e9ccdade5c23f9913e5547
HASAN-SHAIK <skhasanj300@gmail.com>
2022-06-04 19:43:14 +0530

    Included navbar in the app

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Landing from './Landing';
+import { Navbar,Container,Nav, NavLink } from 'react-bootstrap';
+import {Routes,Route} from 'react-router-dom';
+import Viewprofile from './components/Viewprofile';
 
 function App() {
   return (
     <div className="App">
+    <Navbar bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand to="">Team -5</Navbar.Brand>
+      <Nav className="mx -auto">
+        <NavLink className='nav-link' to='redeempoints'>Redeeem points</NavLink>
+        <NavLink className='nav-link' to='viewprofile'>View Profile</NavLink>
+      </Nav>
+      </Container>
+    </Navbar>
       <Landing />
+
+
+    {/* <Routes>
+      <Route path="viewprofile" element={<Viewprofile/>}/>
+    </Routes> */}
+
     </div>
   );
 }
