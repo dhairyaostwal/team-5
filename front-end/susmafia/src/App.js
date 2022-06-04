93df7944d2df8f5f983cb197427cb89a55c65a71
HASAN-SHAIK <skhasanj300@gmail.com>
2022-06-05 00:47:30 +0530

    added tyhe redeem component and profile page

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -5,26 +5,28 @@ import ViewProfile from './components/ViewProfile';
 import Login from './Login';
 import Signup from './Signup';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RedeemPoints from './components/RedeemPoints';
 
 
 function App() {
   return (
-    <Router>   
-      <div className="App">
-      <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/landing" element={<Landing/> }/>
+    <RedeemPoints/>
+  //   <Router>   
+  //     <div className="App">
+  //     <Routes>
+  //       <Route path="/signup" element={<Signup/>}/>
+  //       <Route path="/login" element={<Login/>}/>
+  //       <Route path="/landing" element={<Landing/> }/>
                
         
         
 
 
 
-        <Route path="/" element={<Signup/>}/>
-      </Routes>
-    </div>
-  </Router>
+  //       <Route path="/" element={<Signup/>}/>
+  //     </Routes>
+  //   </div>
+  // </Router>
   );
 }
 
