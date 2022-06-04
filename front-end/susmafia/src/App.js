f84b2679957fad3fa3f52e90ab1704ceb2b3ecd4
Dhairya <dhairya.ostwal@gmail.com>
2022-06-05 00:13:55 +0530

    Frontend: LinkedInFileUpload complete

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Landing from './Landing';
 import NavbarComponent from './components/Navbar';
+import LinkedInFileUpload from './components/LinkedInFileUpload';
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
-      <Landing />
-
+      {/* <NavbarComponent /> */}
+      {/* <Landing /> */}
+      <LinkedInFileUpload />
       {/* <Routes>
       <Route path="viewprofile" element={<Viewprofile/>}/>
     </Routes> */}
