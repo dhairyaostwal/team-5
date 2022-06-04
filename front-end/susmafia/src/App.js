53fb5510762dc587b98a325839605b33bb223eeb
meetagrawal09 <agrawalmeet91@gmail.com>
2022-06-04 23:16:17 +0530

    router-added

--- front-end/susmafia/src/App.js
+++ front-end/susmafia/src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import Landing from './Landing';
 import NavbarComponent from './components/Navbar';
 
+import Login from './Login';
+import Signup from './Signup';
+
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <NavbarComponent />
-      <Landing />
-
-      {/* <Routes>
-      <Route path="viewprofile" element={<Viewprofile/>}/>
-    </Routes> */}
+    <Router>   
+      <div className="App">
+      <Routes>
+        <Route path="/register" element={<Signup/>}/>
+        <Route path="/login" element={<Signup/>}/>
+        <Route path="/" element={<Signup/>}/>
+      </Routes>
     </div>
+  </Router>
   );
 }
 
