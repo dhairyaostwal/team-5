beb0dbcb0512b836d1e8b2918cfb9eeed06830dd
himanshuharlalka <himanshuharlalka@gmail.com>
2022-06-05 07:41:04 +0530

    backend code in viewusers.js

--- front-end/susmafia/src/ViewUsers.js
+++ front-end/susmafia/src/ViewUsers.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 import './ViewUsers.css'
 
 function ViewUsers() {
+
+  const [users,setUsers] = useState([])
+
+  const callAPI = ()=>{
+      const url = 'http://localhost:8000/api/incentives/all-users'
+      fetch(url)
+      .then(res=>res.json())
+      .then(data=>{
+          setUsers(data)
+      console.log(data)})  
+      .catch(err=>console.log(err))
+  }
+
+  useEffect(()=>{
+      callAPI()
+  },[])
   return (
     <div>ViewUsers</div>
   )
